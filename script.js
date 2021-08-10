eb860985a1a09fffa465198280165129951ed316
Devananth <52454081+Devananth@users.noreply.github.com>
2021-08-10 09:31:22 +0530

    Project Uploaded

---
+++ script.js
@@ -0,0 +1,58 @@
+var DOB = document.querySelector('.birth-date');
+
+var NUM = document.querySelector(".lucky-num");
+
+var btn = document.querySelector(".check");
+
+var error = document.querySelector('.error');
+
+var closeNotice = document.querySelector("#close");
+
+closeNotice.addEventListener('click' , () => {
+  document.querySelector('.notice').style.display = "none";
+})
+
+btn.addEventListener('click', () => {
+    
+    clearError();
+
+    var dob = DOB.value;
+
+    var num = NUM.value;
+
+    console.log(num,dob);
+
+    if(dob && num)
+    {
+    }
+    else
+    {
+      showError();
+      return;
+    }
+
+    dob = dob.replaceAll("-");
+
+    var sum = 0;
+
+    for(var n in dob)
+    {
+        sum+=Number(n);
+    }
+
+    num = Math.abs(Number(num));
+
+    if(sum%num==0)
+      document.querySelector('.output').innerHTML = ("Hurrah 🥳 ! Your DOB is lucky");
+    else
+    document.querySelector('.output').innerHTML = ("No , It's not lucky enough ! 😭☹");
+
+});
+
+function showError(){
+  error.style.display = "block";
+}
+
+function clearError(){
+  error.style.display = "none";
+}
